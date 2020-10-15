991b96ed607ab03c854f021a89524e203e914d95
Khalil Belghouat <67176135+KhalilBelghouat@users.noreply.github.com>
2020-10-15 21:01:48 +0100

    Update xgboost.R

--- models/tree_based_methods/xgboost.R
+++ models/tree_based_methods/xgboost.R
@@ -1,15 +1,15 @@
 # SUPPLEMENTARY CODE FOR THE BACHELOR THESIS: 
 # Statistical Learning Approaches to the Socioeconomic Determinants of Social Relegation
 
-# This script applies the xgboost algorithm to the preprocessed dataset.
+# This script applies the XGBoost algorithm to the preprocessed dataset.
 
-# Xgboost training.
-xgboost <- train(y1 ~ ., data = trainset, trControl = trainControl(method = "cv", 
+# XGBoost training.
+XGBoost <- train(y1 ~ ., data = trainset, trControl = trainControl(method = "cv", 
                  number = 10,
                  classProbs = TRUE), 
                  metric = "Kappa",
                  method = "xgbTree")
 
-# Xgboost testing.
-y1_hat <- predict(xgboost, as.data.frame(testset)[-30])
+# XGBoost testing.
+y1_hat <- predict(XGBoost, as.data.frame(testset)[-30])
 confusionMatrix(y1_hat, as.factor(as.data.frame(testset)$y1))
