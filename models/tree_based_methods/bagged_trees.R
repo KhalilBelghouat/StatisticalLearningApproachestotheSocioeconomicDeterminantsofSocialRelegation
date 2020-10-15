fbb20a0fe895a0666c155a8f8d89dd0d252cf84c
Khalil Belghouat <67176135+KhalilBelghouat@users.noreply.github.com>
2020-10-15 19:23:03 +0100

    Update bagged_trees.R

--- models/tree_based_methods/bagged_trees.R
+++ models/tree_based_methods/bagged_trees.R
@@ -3,12 +3,13 @@
 
 # This script applies bagged trees to the preprocessed dataset.
 
-# Bagged trees training
+# Bagged trees training.
 bagged_trees <- train(factor(y1)~., data=trainset, trControl=trainControl(method="cv", 
                        number = 10,
                        classProbs =  TRUE), 
                        metric = "Kappa",
                        method = "treebag")
-# Bagged trees testing
+
+# Bagged trees testing.
 y1_hat <- predict(bagged_trees, as.data.frame(testset)[-30])
 confusionMatrix(y1_hat, as.factor(as.data.frame(testset)$y1))
