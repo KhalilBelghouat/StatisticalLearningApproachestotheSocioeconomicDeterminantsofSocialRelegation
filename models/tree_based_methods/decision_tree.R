3155a1dd866ad4ba987bb8740ec353acef38ddac
Khalil Belghouat <67176135+KhalilBelghouat@users.noreply.github.com>
2020-10-15 17:57:59 +0100

    Update decision_tree.R

--- models/tree_based_methods/decision_tree.R
+++ models/tree_based_methods/decision_tree.R
@@ -1,1 +1,15 @@
+# SUPPLEMENTARY CODE FOR THE BACHELOR THESIS: 
+# Statistical Learning Approaches to the Socioeconomic Determinants of Social Relegation
 
+# This script applies the decision tree algorithm to the preprocessed dataset.
+
+# Decision tree training.
+decision_tree <- train(factor(y1)~., data=trainset, trControl=trainControl(method="cv", 
+                       number = 10,
+                       classProbs = TRUE), 
+                       metric = "Kappa",
+                       method = "rpart")
+
+# Decision tree testing.
+y_pred <- predict(decision_tree, as.data.frame(testset)[-30])
+confusionMatrix(y_pred, as.factor(as.data.frame(testset)$y1))
