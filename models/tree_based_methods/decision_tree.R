d624e2b7de3698d6ddf2720f770d87548ac55bde
Khalil Belghouat <67176135+KhalilBelghouat@users.noreply.github.com>
2020-10-15 19:21:02 +0100

    Update decision_tree.R

--- models/tree_based_methods/decision_tree.R
+++ models/tree_based_methods/decision_tree.R
@@ -3,13 +3,13 @@
 
 # This script applies the decision tree algorithm to the preprocessed dataset.
 
-# Decision tree training.
+# Decision tree training
 decision_tree <- train(factor(y1)~., data=trainset, trControl=trainControl(method="cv", 
                        number = 10,
                        classProbs = TRUE), 
                        metric = "Kappa",
                        method = "rpart")
 
-# Decision tree testing.
+# Decision tree testing
 y1_hat <- predict(decision_tree, as.data.frame(testset)[-30])
 confusionMatrix(y1_hat, as.factor(as.data.frame(testset)$y1))
