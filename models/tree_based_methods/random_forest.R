f42b5861698293effac45cc348688bb3ef5501ae
Khalil Belghouat <67176135+KhalilBelghouat@users.noreply.github.com>
2020-10-15 18:11:44 +0100

    Update random_forest.R

--- models/tree_based_methods/random_forest.R
+++ models/tree_based_methods/random_forest.R
@@ -1,1 +1,15 @@
+# SUPPLEMENTARY CODE FOR THE BACHELOR THESIS: 
+# Statistical Learning Approaches to the Socioeconomic Determinants of Social Relegation
 
+# This script applies random forest to the preprocessed dataset.
+
+# Random forest training.
+random_forest <- train(factor(y1)~., data=trainset, trControl=trainControl(method="cv", 
+                       number = 10,
+                       classProbs =  TRUE), 
+                       metric = "Kappa",
+                       method = "rf")
+
+# Random forest training.
+y1_hat <- predict(random_forest, as.data.frame(testset)[-30])
+confusionMatrix(y1_hat, as.factor(as.data.frame(testset)$y1))
