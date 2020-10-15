fabac4f85772e8f6894072f1d658e4692e0efc58
Khalil Belghouat <67176135+KhalilBelghouat@users.noreply.github.com>
2020-10-15 19:21:47 +0100

    Update random_forest.R

--- models/tree_based_methods/random_forest.R
+++ models/tree_based_methods/random_forest.R
@@ -3,13 +3,13 @@
 
 # This script applies random forest to the preprocessed dataset.
 
-# Random forest training.
+# Random forest training
 random_forest <- train(factor(y1)~., data=trainset, trControl=trainControl(method="cv", 
                        number = 10,
                        classProbs =  TRUE), 
                        metric = "Kappa",
                        method = "rf")
 
-# Random forest training.
+# Random forest training
 y1_hat <- predict(random_forest, as.data.frame(testset)[-30])
 confusionMatrix(y1_hat, as.factor(as.data.frame(testset)$y1))
