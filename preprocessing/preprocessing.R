37c7e03884bff3489f0688acf04c1fcf95e3d4c0
Khalil Belghouat <67176135+KhalilBelghouat@users.noreply.github.com>
2020-10-15 15:28:21 +0100

    Update preprocessing.R

--- preprocessing/preprocessing.R
+++ preprocessing/preprocessing.R
@@ -2,7 +2,8 @@
 # Statistical Learning Approaches to the Socioeconomic Determinants of Social Relegation
 
 # This script passes the social relegation data set through important data preprocessing steps.
-Applying data preprocessing steps to the social relegation dataset.
+
+set.seed(10)
 
 # Loading the data.
 dataset <- read.csv("SRdata.csv")
@@ -15,8 +16,9 @@ which(is.na(dataset), arr.ind=TRUE)
 
 # The dataset contains categorical variables.
 # We replace the missing data at each column by the most frequent column value, i.e., the mode.
+
 # Building the mode function.
-Mode <- function(x){
+mode <- function(x){
     ta = table(x)
     tam = max(ta)
     if (all(ta == tam))
@@ -29,21 +31,35 @@ Mode <- function(x){
     return(mod)
 }
 
-dataset$Francais[is.na(dataset$Francais)] <- as.numeric(Mode(dataset["Francais"]))
+# Replacing the NA values in "Francais" column with its mode.
+dataset$Francais[is.na(dataset$Francais)] <- as.numeric(mode(dataset["Francais"]))
 
-names <- c('Prof_P', 'Edu_P', 'Ed_M', 'Taille_Frat', 'Francais', 'Age_Tranches')
-dataframe[, names] <- lapply(dataframe[, names], as.factor)
-fct_dataframe <- as.data.frame(dataframe)
+# All column variables are encoded as integers.
+# We encode the categorical variables with k > 2 levels as factors.
+# fct, i.e., factors, is R's way of differentiating categorical from numerical variables.
 
-dummy_dataframe <- as.data.frame(predict(dummyVars(~ ., data=fct_dataframe), fct_dataframe))
+# We select the columns of concern and apply the transformation.
+colnames <- c('Prof_P', 'Edu_P', 'Ed_M', 'Taille_Frat', 'Francais', 'Age_Tranches')
+dataset[, colnames] <- lapply(dataset[, colnames], as.factor)
+fct_dataset <- as.data.frame(dataset)
 
-dummy_dataframe$Genre <- mapvalues(dummy_dataframe$Genre, from = c(1, 2), to = c(0, 1))
-dummy_dataframe$Sit_Fami <- mapvalues(dummy_dataframe$Sit_Fami, from = c(1, 2), to = c(0, 1))
-dummy_dataframe$R_C <- mapvalues(dummy_dataframe$R_C, from = c(1, 2), to = c(0, 1))
-dummy_dataframe$R_B <- mapvalues(dummy_dataframe$R_B, from = c(1, 2), to = c(0, 1))
-dummy_dataframe$Etablissemen <- mapvalues(dummy_dataframe$Etablissemen, from = c(1, 2), to = c(0, 1))
-dummy_dataframe$Stage <- mapvalues(dummy_dataframe$Stage, from = c(1, 2), to = c(0, 1))
-dummy_dataframe$Con_Pratiques <- mapvalues(dummy_dataframe$Con_Pratiques, from = c(1, 2), to = c(0, 1))
-dummy_dataframe$Con_informatique <- mapvalues(dummy_dataframe$Con_informatique, from = c(1, 2), to = c(0, 1))
-dummy_dataframe$Con_Commu <- mapvalues(dummy_dataframe$Con_Commu, from = c(1, 2), to = c(0, 1))
-dummy_dataframe$L_anglais <- mapvalues(dummy_dataframe$L_anglais, from = c(1, 2), to = c(0, 1))	
+# Transforming categorical variables into dummy variables.
+dummy_dataset <- as.data.frame(predict(dummyVars(~ ., data=fct_dataset), fct_dataset))
+
+# We apply binary encoding to the categorical variables that weren't affected by earlier encoding.
+dummy_dataset$Genre <- mapvalues(dummy_dataset$Genre, from = c(1, 2), to = c(0, 1))
+dummy_dataset$Sit_Fami <- mapvalues(dummy_dataset$Sit_Fami, from = c(1, 2), to = c(0, 1))
+dummy_dataset$R_C <- mapvalues(dummy_dataset$R_C, from = c(1, 2), to = c(0, 1))
+dummy_dataset$R_B <- mapvalues(dummy_dataset$R_B, from = c(1, 2), to = c(0, 1))
+dummy_dataset$Etablissemen <- mapvalues(dummy_dataset$Etablissemen, from = c(1, 2), to = c(0, 1))
+dummy_dataset$Stage <- mapvalues(dummy_dataset$Stage, from = c(1, 2), to = c(0, 1))
+dummy_dataset$Con_Pratiques <- mapvalues(dummy_dataset$Con_Pratiques, from = c(1, 2), to = c(0, 1))
+dummy_dataset$Con_informatique <- mapvalues(dummy_dataset$Con_informatique, from = c(1, 2), to = c(0, 1))
+dummy_dataset$Con_Commu <- mapvalues(dummy_dataset$Con_Commu, from = c(1, 2), to = c(0, 1))
+dummy_dataset$L_anglais <- mapvalues(dummy_dataset$L_anglais, from = c(1, 2), to = c(0, 1))	
+
+# Schuffling the data.
+rows <- sample(nrow(dummy_dataset))
+dummy_dataset <- dummy_dataset[rows, ]
+dummy_dataset$y1 <- revalue(factor(dummy_dataset$y1), c("0"="non", "1"="oui"))
+head(dummy_dataset)
