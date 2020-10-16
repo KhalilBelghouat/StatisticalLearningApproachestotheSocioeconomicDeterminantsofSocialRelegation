9a5a7ee89aa16ca08cd6b637e145c929d3022fce
Khalil Belghouat <67176135+KhalilBelghouat@users.noreply.github.com>
2020-10-16 04:39:18 +0100

    Update SHAP_ xgboost.R

--- SHAP/SHAP_tree_based_methods/SHAP_ xgboost.R
+++ SHAP/SHAP_tree_based_methods/SHAP_ xgboost.R
@@ -1,1 +1,15 @@
+# SUPPLEMENTARY CODE FOR THE BACHELOR THESIS: 
+# Statistical Learning Approaches to the Socioeconomic Determinants of Social Relegation
 
+# This script applies bagged trees to the preprocessed dataset.
+
+# Bagged trees training.
+bagged_trees <- train(y1 ~ ., data = trainset, trControl = trainControl(method = "cv", 
+                      number = 10,
+                      classProbs = TRUE), 
+                      metric = "Kappa",
+                      method = "treebag")
+
+# Bagged trees testing.
+y1_hat <- predict(bagged_trees, as.data.frame(testset)[-30])
+confusionMatrix(y1_hat, as.factor(as.data.frame(testset)$y1))
