46f3c8adb1afcb81c973cb06829460f9667f02d6
Khalil Belghouat <67176135+KhalilBelghouat@users.noreply.github.com>
2020-10-18 10:06:05 +0100

    Update ridge.R

--- models/penalized_logistic_regression/ridge.R
+++ models/penalized_logistic_regression/ridge.R
@@ -13,13 +13,13 @@ trainset <- fct_dataset[1:1300, ]
 testset <- fct_dataset[1301:1621, ]
 
 # Ridge logistic regression training.
-ridge <- train(y1 ~ ., data = trainset, trControl = trainControl(method = "cv", 
-               number = 10),
-               method = "glmnet",
-               family = "binomial",
-               metric = "Kappa",
-               tuneGrid = expand.grid(alpha = 0,
-               lambda = seq(0.01, 0.1, by = 0.001)))
+ridge <- caret::train(y1 ~ ., data = trainset, trControl = trainControl(method = "cv", 
+                      number = 10),
+                      method = "glmnet",
+                      family = "binomial",
+                      metric = "Kappa",
+                      tuneGrid = expand.grid(alpha = 0,
+                      lambda = seq(0.01, 0.1, by = 0.001)))
 
 # Ridge logistic regression testing.
 y1_hat <- predict(ridge, as.data.frame(testset)[-30])
