543909622ee4a878dd857e37d304376e4c478ab8
Khalil Belghouat <67176135+KhalilBelghouat@users.noreply.github.com>
2020-10-18 10:05:12 +0100

    Update lasso.R

--- models/penalized_logistic_regression/lasso.R
+++ models/penalized_logistic_regression/lasso.R
@@ -13,13 +13,13 @@ trainset <- fct_dataset[1:1300, ]
 testset <- fct_dataset[1301:1621, ]
 
 # Lasso logistic regression training.
-lasso <- train(y1 ~ ., data = trainset, trControl = trainControl(method = "cv", 
-               number = 10),
-               method = "glmnet",
-               family = "binomial",
-               metric = "Kappa",
-               tuneGrid = expand.grid(alpha = 1,
-               lambda = seq(0.01, 0.1, by = 0.001)))
+lasso <- caret::train(y1 ~ ., data = trainset, trControl = trainControl(method = "cv", 
+                      number = 10),
+                      method = "glmnet",
+                      family = "binomial",
+                      metric = "Kappa",
+                      tuneGrid = expand.grid(alpha = 1,
+                      lambda = seq(0.01, 0.1, by = 0.001)))
 
 # Lasso logistic regression testing.
 y1_hat <- predict(lasso, as.data.frame(testset)[-30])
