d3944b9318d831c289242f455bc1db7787f62c6f
Khalil Belghouat <67176135+KhalilBelghouat@users.noreply.github.com>
2020-10-18 09:59:50 +0100

    Update neural_network.R

--- models/neural_network/neural_network.R
+++ models/neural_network/neural_network.R
@@ -3,15 +3,17 @@
 
 # This script applies a neural network to the preprocessed dataset.
 
+source('preprocessing.R')
+
 # Neural network training.
-neural_network <- train(y1 ~ ., data = trainset, trControl = trainControl(method = "cv",
-                        number = 10,
-                        classProbs = TRUE),
-                        tuneGrid = expand.grid(layer1 = 25,
-                                                layer2 = 25,
-                                                layer3 = 25), 
-                        metric = "Kappa",
-                        method = "mlpML")
+neural_network <- caret::train(y1 ~ ., data = trainset, trControl = trainControl(method = "cv",
+                               number = 10,
+                               classProbs = TRUE),
+                               tuneGrid = expand.grid(layer1 = 25,
+                                                      layer2 = 25,
+                                                      layer3 = 25), 
+                               metric = "Kappa",
+                               method = "mlpML")
 
 # Neural network testing.
 y1_hat <- predict(neural_network, as.data.frame(testset)[-30])
